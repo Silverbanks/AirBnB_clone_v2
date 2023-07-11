54983dc6b06fe6d248b325f8876334e128031e8d
Silverbanks <74558088+TheVoltron@users.noreply.github.com>
2023-07-11 04:14:17 +0100

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,21 +2,29 @@
 # Set up server file system for deployment
 
 # install nginx
-sudo apt-get -y update
-sudo apt-get -y install nginx
-sudo service nginx start
+apt-get -y update
+apt-get -y install nginx
+service nginx start
 
 # configure file system
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html > /dev/null
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html > /dev/null
+sed -i '/server_name _/a location /redirect_me { rewrite ^ https://www.youtube.com/@silver_banks permanent; }' /etc/nginx/sites-available/default
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # set permissions
-sudo chown -R ubuntu:ubuntu /data/
+chown -R ubuntu:ubuntu /data/
 
 # configure nginx
-sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sed -i '/listen 80 default_server/a location /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 
 # restart web server
-sudo service nginx restart
+service nginx restart
